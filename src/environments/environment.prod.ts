4d02f3e0dcc3efd8513282ac62405c92854c6f89
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2025-09-05 18:05:39 -0500

    Fx:Coordenadas de municipios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://yamidapp-backend.onrender.com/api'
-};
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://yamidapp-backend.onrender.com/api'
+//   apiUrl: 'http://localhost:3000/api'
+
+// };
